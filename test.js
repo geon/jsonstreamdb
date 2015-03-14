c82e92cfc7b7bff4be131b7e888562bb4740f3fb
Victor Widell <victor@topmost.se>
2015-03-14 09:22:56 +0100

    Fixed case.

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 
 var Stream = require('./stream');
-var TestObjectGenerator = require('./test-object-generator');
+var testObjectGenerator = require('./test-object-generator');
 
 console.log('generateTestStream');
 
-var stream = TestObjectGenerator.generateTestStream(10000);
+var stream = testObjectGenerator.generateTestStream(10000);
 
 console.log('aggregate');
 
--- test2.js
+++ test2.js
@@ -1,8 +1,8 @@
 
 var Stream = require('./stream');
-var TestObjectGenerator = require('./test-object-generator');
+var testObjectGenerator = require('./test-object-generator');
 var fs = require('fs');
 
-var stream = TestObjectGenerator.generateTestStream(10000);
+var stream = testObjectGenerator.generateTestStream(10000);
 
 fs.writeFileSync('test.jsonstream', Stream.jsonObjectToJsonStream(stream));
