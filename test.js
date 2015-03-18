3d20e2c79dd36b453b75c7ecfe9e5315ff5a4eba
Victor Widell <victor@topmost.se>
2015-03-18 18:09:50 +0100

    Updated test.

--- test.js
+++ test.js
@@ -1,15 +1,30 @@
 
 var StreamDb = require('./StreamDb.js');
+var StreamDbState = require('./StreamDbState.js');
 var testObjectGenerator = require('./test-object-generator');
+var JsonStreamSerializer = require('./JsonStreamSerializer');
 
 
-var db = new StreamDb(null, {aggregate: true});
+var db = new StreamDb('test.jsonstreamdb');
+// var aggregate = new StreamDbState(db);
 
-testObjectGenerator.generateTestStream(10000)
-	.pipe(db);
+setInterval(function () {
 
-db.once('finish', function () {
+	testObjectGenerator.generateTestStream(1)
+		.pipe(db, {end: false});
+}, 500);
 
-	console.log(Object.keys(db.aggregate.topics).length);
-	console.log(db.updates.length);
-});
+
+db
+	.pipe(new JsonStreamSerializer(), {history: true})
+	.pipe(process.stdout);
+
+
+// db.pipe(aggregate, {history: true});
+
+// setTimeout(function () {
+
+// 	console.log(Object.keys(aggregate.topics).length);
+// 	// console.log(aggregate.topics);
+
+// }, 500);
