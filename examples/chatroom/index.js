b8325e8759eda82e5f5d8f2c09deeeb49d08cbb6
Victor Widell <victor@topmost.se>
2016-03-26 15:47:45 +0100

    Cleanup, comments.

--- examples/chatroom/index.js
+++ examples/chatroom/index.js
@@ -9,24 +9,23 @@ var WebsocketStream = require('./WebsocketStream.js');
 var ChatClient = require('./ChatClient.js');
 
 
-
+// Serve the client static file.
 var httpServer = http.createServer(function (request, response) {
-
 
 	fs.createReadStream('client.html').pipe(response)
 });
-
 httpServer.listen(3000);
 
 
+// The chatlog.
+var db = new JsonStreamDb('chatlog.jsonstreamdb');
 
+
+// Set up the WS server.
 wsServer = new WebSocketServer({
 	httpServer: httpServer,
 	autoAcceptConnections: true
 });
-
-var db = new JsonStreamDb('chatlog.jsonstreamdb');
-
 wsServer.on('connect', function(connection) {
 
 	var chatClient = new ChatClient(
